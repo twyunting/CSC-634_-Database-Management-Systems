cd2a27fdb0c102d26b273c45d1af36994b95fcf7
twyunting <yc6705a@american.edu>
2021-05-17 16:15:22 -0400

    Create wk1.sql

---
+++ labs/wk1_mon/wk1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE summerDB;
+USE summerDB;
+CREATE TABLE myMessages (message varchar(50));
+
+INSERT INTO myMessages VALUES ("This is the summer 2021 class");
+SELECT * FROM myMessages;
